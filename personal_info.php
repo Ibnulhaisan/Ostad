0de8c484a3d06e860042416f16472ffe57f30451
Ibnulhaisan <60622364+Ibnulhaisan@users.noreply.github.com>
2023-09-13 13:31:16 +0600

    Task4

---
+++ personal_info.php
@@ -0,0 +1,15 @@
+   <?php
+ 
+    $name = "Kazi Ibnul Haisan";
+    $age = 25; 
+    $country = "Bangladesh";
+    $introduction = "Hello, I'm $name. I'm $age years old and I come from $country.";
+
+  
+    echo "<h1>Personal Information</h1>";
+    echo "<p><strong>Name:</strong> $name</p>";
+    echo "<p><strong>Age:</strong> $age</p>";
+    echo "<p><strong>Country:</strong> $country</p>";
+    echo "<p><strong>Introduction:</strong> $introduction</p>";
+    ?>
+
